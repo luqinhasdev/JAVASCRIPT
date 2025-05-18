967c84359cb0d7422f26787b8c298e44e0b46ac9
Lucas <lucasweidmann08@gmail.com>
2025-05-18 13:35:15 -0300

    ESMODULES STUDYING

---
+++ javascript-studying/JSStudying/ESModules.js
@@ -0,0 +1,22 @@
+// Named Export
+
+export function sum(a, b) {
+    // 
+}
+
+import { sum } from ESModules.js
+import { sum as soma } from ESModules.js
+
+//
+
+// Export Default
+
+export default function sum(a, b) {
+    // 
+}
+
+import soma from ESModules.js // FUNÇÃO RENOMEADA
+
+// Export *
+
+import * as math from ESModules.js
