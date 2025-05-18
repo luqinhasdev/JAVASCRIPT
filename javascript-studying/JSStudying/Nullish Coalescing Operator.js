9421ad5bb510ff67c6079692fb95b48a5ceb2987
Lucas <lucasweidmann08@gmail.com>
2025-05-18 13:36:46 -0300

    NULLISH COALESCING OPERATOR STUDYING

---
+++ javascript-studying/JSStudying/Nullish Coalescing Operator.js
@@ -0,0 +1,8 @@
+// Nullish Coalescing Operator
+
+const number = null;
+
+document.body.innerText = "Your Age: " + (number ?? "UNDEFINED");
+
+// THE ?? OPERATOR RETURNS THE RIGHT HAND SIDE VALUE IF THE LEFT HAND SIDE IS NULL OR UNDEFINED.
+// THE ?? OPERATOR RETURNS THE RIGHT HAND SIDE VALUE IF THE LEFT HAND SIDE IS FALSY.
